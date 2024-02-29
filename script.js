7df4191c6a06fc98334553c57c14073694644dad
titoszek <113134176+titoszek@users.noreply.github.com>
2024-02-29 20:13:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function z35() {
+    window.open("z_35_Dzie.html", "okienko_z28","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z36() {
+    window.open("z_36_Dzie.html", "okienko_z29","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z38() {
+    window.open("z_38_Dzie.html", "okienko_z30","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z38_w() {
+    window.open("z_38w_Dzie.html", "okienko_z31","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z39() {
+    window.open("z_39_Dzie.html", "okienko_z32","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ function z40() {
+    window.open("z_40_Dzie.html", "okienko_z30_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
